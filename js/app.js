253ef6a7104128936fb0d8875f08d5f71d8115c2
moamen <moamenalrejjal@gmail.com>
2022-01-23 09:54:05 +0200

    todojs

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var name = prompt("please enter your name ");
+var grnder = prompt("please enter your gender (male or female) without space");
+var age = prompt("please enter your age");
+var confirm = prompt("do you want to skip the welcoming message");
+
+age = prompt("how old are you ?");
+
+//parseInt(age) <= 0 ? alert("your age is small \n you can back later"):confirm(`welcome ${name} \n press yes to move the welcomeing message`);
+
+//grnder.toLowerCase() === "male" ? nickName : grnder.toLowerCase() === "female" ?
+//(nickName = "Msr") : (errorMsg = "you entered answer wrong :(");
+ //errorMsg != "" ?
+ //alert (`welcome ${name}`):
+ //alert (`welcome again ${name}`);
