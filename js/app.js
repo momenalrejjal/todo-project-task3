8066326a21e7cff0fba8d5d3770a180d2b2752a8
moamen <moamenalrejjal@gmail.com>
2022-01-24 10:09:32 +0200

    todojs2

--- js/app.js
+++ js/app.js
@@ -1,14 +1,49 @@
-var name = prompt("please enter your name ");
-var grnder = prompt("please enter your gender (male or female) without space");
-var age = prompt("please enter your age");
+/*var username = prompt("please enter your name ");
+var gender = prompt("please enter your gender (male or female) without space");
+if(gender.toLowerCase() !== "male" && gender.toLowerCase() !== "female"){
+ alert("gender input is not correct");}
+
+var age = prompt("how old are you ?");
+if(parseInt(age) <=0){
+    alert("your age is small \n you can back later");
+}
 var confirm = prompt("do you want to skip the welcoming message");
 
-age = prompt("how old are you ?");
+ if(gender === "male"){
+    alert (`welcome Mr ${username}`);
+ }
+    if(gender === "female"){
+    alert (`welcome Ms ${username}`);
 
-//parseInt(age) <= 0 ? alert("your age is small \n you can back later"):confirm(`welcome ${name} \n press yes to move the welcomeing message`);
+ }
+ else{ alert (`welcome`);}*/
 
-//grnder.toLowerCase() === "male" ? nickName : grnder.toLowerCase() === "female" ?
-//(nickName = "Msr") : (errorMsg = "you entered answer wrong :(");
- //errorMsg != "" ?
- //alert (`welcome ${name}`):
- //alert (`welcome again ${name}`);
+//task6 code
+
+function askqus(question){
+   let answer = prompt(question);
+   if(answer == "yes" || "no"){
+      return answer;
+   }else if (answe == " "){
+      return "invalid";
+   }
+}
+
+let qus1 = prompt("do you have asister?");
+let qus2 = prompt("do you have a brother?");
+let qus3 = prompt("do you enjoy drinking coffee?");
+
+let answersArr = [qus1 ,qus2 ,qus3];
+
+console.log(answersArr);
+
+for(let i=0; i<answersArr.length; i++){
+   console.log(answersArr);
+}
+let traverse = (arr) =>{
+   for(let i=0; i<arr.length; i++){
+      console.log(arr[i]);
+   }
+}
+
+
