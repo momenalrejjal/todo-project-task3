2698b209886e3b0c709a5a5a4f8759bd785a5a15
moamen <moamenalrejjal@gmail.com>
2022-02-15 03:15:40 +0200

    updated

--- js/app.js
+++ js/app.js
@@ -1,85 +1,82 @@
 
 var username = prompt("please enter your name ");
 var gender = prompt("please enter your gender (male or female) without space");
-if(gender.toLowerCase() !== "male" && gender.toLowerCase() !== "female"){
- alert("gender input is not correct");}
 
 var age = prompt("how old are you ?");
-if(parseInt(age) <=0){
-    alert("your age is small \n you can back later");
-}
+
 var confirm = prompt("do you want to skip the welcoming message");
- if(gender === "male"){
-    alert (`welcome Mr ${username}`);
- }else
-    if(gender === "female"){
-    alert (`welcome Ms ${username}`);
- }
- else{ alert (`welcome`);}
 
- var qus1 = prompt("do you have asister?");
- if(qus1 === "yes" || qus1 === "no"){
-alert(`nice!`);
- }else{alert(`invalid`);
-qus1 = "invalid";
-}
- var qus2 = prompt("do you have a brother?");
- if(qus2 === "yes" || qus2 === "no"){
-alert(`nice!`)
-}else{alert(`invalid`);
-qus2 = "invalid";
-}
-  var qus3 = prompt("do you enjoy drinking coffee?");
-  if(qus3 === "yes" || qus3 === "no"){
-alert(`nice!`);
-}else{alert(`invalid`);
-qus3 = "invalid";
+var qus1 = prompt("do you have asister?");
+
+var qus2 = prompt("do you have a brother?");
+
+var qus3 = prompt("do you enjoy drinking coffee?");
+
+
+
+function checkgendercorrect(gender) {
+   if (gender.toLowerCase() !== "male" && gender.toLowerCase() !== "female") {
+      return ("gender input is not correct");
+   }
 }
 
- //function askqus(question){
-   //let answer = prompt(question);
-   //if(answer == "yes" || "no"){
-     // return answer;
-   //}else if (answe == " "){
-     // return prompt("invalid");
-   //}
-//}
+function checkage(age) {
+   if (parseInt(age) <= 0) {
+      return alert("your age is small \n you can back later");
+   }
+}
 
- let answersArr = [qus1 ,qus2 ,qus3];
+function checkcomfirm(gender) {
+   if (gender === "male") {
+      return alert(`welcome Mr ${username}`);
+   } else
+      if (gender === "female") {
+         return alert(`welcome Ms ${username}`);
+      }
+      else { return alert(`welcome`); }
+}
 
+function checkqus1(qus1) {
+   if (qus1 === "yes" || qus1 === "no") {
+       alert(`nice!`);
+   } else {  alert(`invalid`); }
+
+ return qus1 = "invalid";
+}
+
+function qus2 (qus2){
+if (qus2 === "yes" || qus2 === "no") {
+   alert(`nice!`)
+} else {
+   alert(`invalid`);
+  return qus2 = "invalid";
+}}
+
+function qus3(qus3){
+if (qus3 === "yes" || qus3 === "no") {
+   alert(`nice!`);
+} else {
+   alert(`invalid`);
+ return  qus3 = "invalid";
+}
+}
+
+function answers(qus1, qus2 ,qus3){
+let answersArr = [qus1, qus2, qus3];
 answersArr.reverse();
-
-console.log(answersArr);
-
- 
-
-
-
-
-
- 
-
-
-
-var age = prompt("how old are you ?");
-if(parseInt(age) <=0){
-    alert("your age is small \n you can back later");
+return console.log(answersArr);
 }
-var confirm = prompt("do you want to skip the welcoming message");
-//parseInt(age) <= 0 ? alert("your age is small \n you can back later"):confirm(`welcome ${username} \n press yes to move the welcomeing message`);
-//gender.toLowerCase() === "male" ? nickName : gender.toLowerCase() === "female" ?
-//(nickName = "Msr") : (errorMsg = "you entered answer wrong :(");
- //errorMsg != "" ?
- if(gender === "male"){
-    alert (`welcome Mr ${username}`);
- }break;
-    if(gender === "female"){
-    alert (`welcome Ms ${username}`);
-
- }
- else{ alert (`welcome`);}
 
 
-//Alert a welcoming message with the name of the user and the title Mr if the user is male and Ms if the user is female, 
-//and if the gender input is not correct alert the welcoming message without the title, according to the confirmation
+function checkgender(gender){
+if (gender === "male") {
+   alert(`welcome Mr ${username}`);
+} break;
+if (gender === "female") {
+   alert(`welcome Ms ${username}`);
+
+}
+else { alert(`welcome`); }
+}
+
 
